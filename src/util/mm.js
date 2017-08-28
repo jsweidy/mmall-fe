7c211bcd0d45d9ea33488c4bf07297e7b8c76b00
jsweidy <jsweidy@126.com>
2017-08-28 11:53:54 +0800

    用户模块

--- src/util/mm.js
+++ src/util/mm.js
@@ -71,14 +71,14 @@ var _mm = {
 			return /^1\d{10}$/.test(value);
 		}
 		//邮箱格式验证
-		if('phone' === type){
+		if('email' === type){
 			return /^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/.test(value);
 		}
 	},
 
 	//统一登录处理
 	doLogin: function(){
-		window.location.href = './login.html?redirect=' + encodeURIComponent(window.location.href);
+		window.location.href = './user-login.html?redirect=' + encodeURIComponent(window.location.href);
 	},
 	goHome: function(){
 		window.location.href = './index.html';
