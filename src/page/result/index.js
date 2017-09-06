c2dfb2acb958a0de68ea86740aace89e3d529a94
jsweidy <jsweidy@126.com>
2017-09-06 11:34:06 +0800

    payment

--- src/page/result/index.js
+++ src/page/result/index.js
@@ -7,4 +7,9 @@ $(function(){
 	var type       = _mm.getUrlParam('type') || 'default';
 	var $element   = $('.'+ type + '-success');
 	$element.show();
+
+	if(type === 'payment'){
+		var $orderNumber = $element.find('.order-number');
+		$orderNumber.attr('href' , $orderNumber.attr('href')+_mm.getUrlParam('orderNumber'));
+	}
 })
